fd5e7c9563bc5b85472129932ae40ba0bd540b33
pimenta <gustavot.pimenta@yahoo.com.br>
2026-06-30 16:37:11 -0400

    .

--- commit_git.sh
+++ commit_git.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-git add .
+set -e
 
+echo "================================="
+echo "      MAGNOCYBER GIT PUSH"
+echo "================================="
+
+echo
 git status
 
 echo
 read -p "Commit message: " MSG
 
+git add .
 git commit -m "$MSG"
-
 git push origin main
+
+echo
+echo "[+] Commit enviado com sucesso."
